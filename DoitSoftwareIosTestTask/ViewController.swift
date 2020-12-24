6dfdd490bfbac7c2c3292214c88e7526d25ef2e4
Ihor Myroniuk <ihormyroniuk@gmail.com>
2020-12-25 00:15:22 +0200

    Create Xcode "App" project

---
+++ DoitSoftwareIosTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoitSoftwareIosTestTask
+//
+//  Created by Ihor Myroniuk on 25.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
